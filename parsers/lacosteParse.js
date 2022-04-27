227f6f46478c47616b13bb936046d79d8d11e10c
Vyacheslav <arefievvyacheslavspb@gmail.com>
2022-04-27 16:15:54 +0300

    сделал парсер lacoste

--- parsers/lacosteParse.js
+++ parsers/lacosteParse.js
@@ -17,9 +17,13 @@ module.exports = function lacosteParse(itemArr) {
                 // 2.4. Получаю категорию
                 if (item.categoryId.includes('/')) {
                     item.categoryId = item.categoryId.split('/');
+                    item.category = item.categoryId[item.categoryId.length - 2]
                     item.categoryId = item.categoryId[item.categoryId.length - 1]
                 }
                 if (item.categoryId === 'Поло') item.categoryId = 'Футболки Поло'
+                if (item.categoryId === 'Спортивная одежда') item.categoryId = 'Штаны'
+                if (item.categoryId === 'Верхняя одежда') item.categoryId = 'Куртки'
+                if (item.categoryId === 'Сумки и кошельки') item.categoryId = 'Сумки, Рюкзаки и Кошельки'
 
                 // 2.5. Прохожусь по каждому параметру
                 if (!!item.param && item.param.includes('|')) {
@@ -74,8 +78,6 @@ module.exports = function lacosteParse(itemArr) {
                                 } else item.params.size = paramArr
                             }
 
-
-
                             if (item.categoryId === 'Футболки' || item.categoryId === 'Футболки Поло') {
 
                                 const sizesTShortsObj = {
@@ -102,9 +104,9 @@ module.exports = function lacosteParse(itemArr) {
 
                                     for (let i = +paramArr[0]; i <= +paramArr[paramArr.length - 1]; i++) {
 
-                                        Object.keys(sizesShortsObj).forEach(size => {
+                                        Object.keys(sizesTShortsObj).forEach(size => {
 
-                                            if (i === +size) resultParamArr.push(sizesShortsObj[size])
+                                            if (i === +size) resultParamArr.push(sizesTShortsObj[size])
                                         })
                                     }
 
@@ -112,56 +114,203 @@ module.exports = function lacosteParse(itemArr) {
                                 } else item.params.size = paramArr
                             }
 
-
-
                             if (item.categoryId === 'Толстовки') {
 
-                                // const sizesHoodiesObj = {
-                                //     2: 'XS',
-                                //     3: 'S',
-                                //     4: 'M',
-                                //     5: 'M-L',
-                                //     6: 'L',
-                                //     7: 'XL',
-                                //     34: 'XXXS',
-                                //     36: 'XXS',
-                                //     38: 'XS',
-                                //     40: 'S',
-                                //     42: 'XXS',
-                                //     44: 'XS',
-                                //     46: 'S',
-                                //     48: 'M',
-                                //     50: 'L',
-                                //     52: 'XL',
-                                //     54: 'XXL',
-                                // }
-                                //
-                                // if (!isNaN(paramArr[0])) {
-                                //
-                                //     for (let i = +paramArr[0]; i <= +paramArr[paramArr.length - 1]; i++) {
-                                //
-                                //         Object.keys(sizesShortsObj).forEach(size => {
-                                //
-                                //             if (i === +size) resultParamArr.push(sizesShortsObj[size])
-                                //         })
-                                //     }
-                                //
-                                //     item.params.size = resultParamArr
-                                // } else item.params.size = paramArr
-                                //
-                                // console.log(item.params.size);
+                                const sizesHoodiesObj = {
+                                    2: 'XS',
+                                    3: 'S',
+                                    4: 'M',
+                                    5: 'M-L',
+                                    6: 'L',
+                                    7: 'XL',
+                                    34: 'XXXS',
+                                    36: 'XXS',
+                                    38: 'XS',
+                                    40: 'S',
+                                    42: 'XXS',
+                                    44: 'XS',
+                                    46: 'S',
+                                    48: 'M',
+                                    50: 'L',
+                                    52: 'XL',
+                                    54: 'XXL',
+                                }
+
+                                if (!isNaN(paramArr[0])) {
+
+                                    for (let i = +paramArr[0]; i <= +paramArr[paramArr.length - 1]; i++) {
+
+                                        Object.keys(sizesHoodiesObj).forEach(size => {
+
+                                            if (i === +size) resultParamArr.push(sizesHoodiesObj[size])
+                                        })
+                                    }
+
+                                    item.params.size = resultParamArr
+                                } else item.params.size = paramArr
                             }
 
+                            if (item.categoryId === 'Штаны' || item.categoryId === 'Брюки') {
 
+                                const sizesTrousersObj = {
+                                    2: 'XXS',
+                                    3: 'XXS',
+                                    4: 'XS',
+                                    5: 'XS',
+                                    6: 'S',
+                                    7: 'S',
+                                    8: 'M',
+                                    32: 'XXS',
+                                    34: 'XS',
+                                    36: 'S',
+                                    38: 'M',
+                                    40: 'M',
+                                    42: 'L',
+                                    44: 'L'
+                                }
 
+                                if (!isNaN(paramArr[0])) {
 
+                                    for (let i = +paramArr[0]; i <= +paramArr[paramArr.length - 1]; i++) {
 
+                                        Object.keys(sizesTrousersObj).forEach(size => {
 
+                                            if (i === +size && !resultParamArr.includes(sizesTrousersObj[size])) resultParamArr.push(sizesTrousersObj[size])
+                                        })
+                                    }
 
+                                    item.params.size = resultParamArr
+                                } else item.params.size = paramArr
+                            }
 
+                            if (item.categoryId === 'Платья и юбки') {
 
+                                const sizesDressesObj = {
+                                    32: 'XXS',
+                                    34: 'XS',
+                                    36: 'S',
+                                    38: 'M',
+                                    40: 'M',
+                                    42: 'L',
+                                    44: 'L'
+                                }
 
+                                if (!isNaN(paramArr[0])) {
+
+                                    for (let i = +paramArr[0]; i <= +paramArr[paramArr.length - 1]; i++) {
+
+                                        Object.keys(sizesDressesObj).forEach(size => {
+
+                                            if (i === +size) resultParamArr.push(sizesDressesObj[size])
+                                        })
+                                    }
+
+                                    item.params.size = resultParamArr
+                                } else item.params.size = paramArr
+                            }
+
+                            if (item.categoryId === 'Куртки') {
+
+                                const sizesJacketsObj = {
+                                    32: 'XXS',
+                                    34: 'XS',
+                                    36: 'S',
+                                    38: 'M',
+                                    40: 'M',
+                                    42: 'L',
+                                    44: 'L',
+                                    46: 'S',
+                                    48: 'M',
+                                    50: 'L',
+                                    52: 'XL',
+                                    54: 'XXL',
+                                    56: 'XXXL',
+                                    58: 'XXXXL'
+                                }
+
+                                if (!isNaN(paramArr[0])) {
+
+                                    for (let i = +paramArr[0]; i <= +paramArr[paramArr.length - 1]; i++) {
+
+                                        Object.keys(sizesJacketsObj).forEach(size => {
+
+                                            if (i === +size) resultParamArr.push(sizesJacketsObj[size])
+                                        })
+                                    }
+
+                                    item.params.size = resultParamArr
+                                } else item.params.size = paramArr
+                            }
+
+                            if (item.categoryId === 'Блузы и рубашки' || item.categoryId === 'Блузы и рубашки') {
+
+                                const sizesShirtsObj = {
+                                    32: 'XXS',
+                                    34: 'XS',
+                                    36: 'S',
+                                    38: 'M',
+                                    40: 'M',
+                                    42: 'L',
+                                    44: 'L',
+                                    46: 'S',
+                                    48: 'M',
+                                    50: 'L',
+                                    52: 'XL',
+                                    54: 'XXL',
+                                    56: 'XXXL'
+                                }
+
+                                if (!isNaN(paramArr[0])) {
+
+                                    for (let i = +paramArr[0]; i <= +paramArr[paramArr.length - 1]; i++) {
+
+                                        Object.keys(sizesShirtsObj).forEach(size => {
+
+                                            if (i === +size) resultParamArr.push(sizesShirtsObj[size])
+                                        })
+                                    }
+
+                                    item.params.size = resultParamArr
+                                } else item.params.size = paramArr
+                            }
+
+                            if (item.categoryId === 'Кроссовки' || item.categoryId === 'Кеды') {
+
+                                const sizesSneakersObj = {
+                                    4: '36.5',
+                                    5: '37.5',
+                                    6: '38',
+                                    6.5: '38.5',
+                                    7: '39',
+                                    7.5: '39.5',
+                                    8: '40',
+                                    8.5: '40.5',
+                                    9: '41',
+                                    9.5: '41.5',
+                                    10: '42',
+                                    10.5: '42.5',
+                                    11: '43',
+                                    11.5: '43.5',
+                                    12: '44',
+                                    12.5: '44.5',
+                                    13: '45'
+                                }
+
+                                if (!isNaN(paramArr[0])) {
+
+                                    for (let i = +paramArr[0]; i <= +paramArr[paramArr.length - 1]; i++) {
+
+                                        Object.keys(sizesSneakersObj).forEach(size => {
+
+                                            if (i === +size) resultParamArr.push(sizesSneakersObj[size])
+                                        })
+                                    }
+
+                                    item.params.size = resultParamArr
+                                } else item.params.size = paramArr
+                            }
                         }
+
                         if (oneParamArr[0] === 'Размер' && !oneParamArr[1].includes(',')) item.params.size = [...oneParamArr[1]]
 
                         if (oneParamArr[0] === 'Размер') {
@@ -176,8 +325,6 @@ module.exports = function lacosteParse(itemArr) {
                     })
                 }
             })
-
-            // console.log(itemArr);
 
             resolve(itemArr)
 
