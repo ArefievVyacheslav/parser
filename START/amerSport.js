8019f0625a35b42260593f624899108c6055b440
Arefiev Vyacheslav <arefievvyacheslavspb@gmail.com>
2022-09-04 14:41:58 +0300

    fp

--- START/amerSport.js
+++ START/amerSport.js
@@ -9,7 +9,7 @@ let token = null;
 let productsArrJSON = [];
 
 
-const amerSport = 'https://export.admitad.com/ru/webmaster/websites/2276598/products/export_adv_products/?template=66341&user=arefievvyacheslavspb%40gmail.com&code=btg1ajk9az&feed_id=7775';
+const amerSport = 'https://export.admitad.com/ru/webmaster/websites/2276598/products/export_adv_products/?template=66341&user=arefievvyacheslavspb%40gmail.com&code=btg1ajk9az&feed_id=18974';
 
 (async function start() {                                                                                                    // Запуск парсер
     await Auth().then(tokenRes => token = tokenRes)                                                                         // Цикл по магазам
